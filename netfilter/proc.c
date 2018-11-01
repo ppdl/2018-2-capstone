a485471e9ef2a6f46bb6c79e9607dafd6d5844da
YJ Cho <4nuri1205@naver.com>
2018-11-01 19:12:33 +0900

    server.py added

--- netfilter/proc.c
+++
@@ -1,4 +0,0 @@
-#include <linux/module.h>
-#include <linux/proc_fs.h>
-#include <linux/seq_file.h>
-
---
+++ simpleServer/server.py
@@ -0,0 +1,23 @@
+import socket
+import time
+
+HOST=''
+PORT=54321
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("listening")
+    s.bind((HOST,PORT))
+    s.listen()
+    while True:
+        s.listen()
+
+        conn,addr=s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                print("length: " + str(len(data)))
+                if not data:
+                    break
+            print('Completed\n')
+
